e175aa8bb10d8e10698bed8b71d2da10144e94a6
markew <mark.woolston@axon.com.au>
2012-07-26 07:56:46 +0000

    add species lookups
    git-svn-id: http://ala-expert.googlecode.com/svn/trunk/expert@8 acd0ad0d-b1c6-e151-42b4-198cfe2acdb2

--- grails-app/services/au/org/ala/expert/BieService.groovy
+++ grails-app/services/au/org/ala/expert/BieService.groovy
@@ -2,7 +2,6 @@ package au.org.ala.expert
 
 import org.codehaus.groovy.grails.commons.ConfigurationHolder
 import grails.converters.JSON
-import net.sf.json.JSONNull
 
 class BieService {
 
@@ -21,51 +20,74 @@ class BieService {
 
         // list of guids to look up
         def familyGuids = []
+        def speciesGuids = []
 
         // get unique families + known metadata
         def uniqueByFamily = list.clone().unique({it.family})
         uniqueByFamily.each {
             families.put it.family, [
                     guid: it.familyGuid,
+                    anySpeciesGuid: it.guid,
                     caabCode: it.familyCaabCode
             ]
-            familyGuids << it.familyGuid
+            if (ConfigurationHolder.config.expert.images.useConstructedUrls) {
+                families[it.family].image = [largeImageUrl: ConfigurationHolder.config.bie.baseURL +
+                        '/species/image/large/' + it.guid]
+            }
+            if (it.familyGuid) {
+                familyGuids << it.familyGuid
+            }
+            if (it.guid) {
+                speciesGuids << it.guid
+            }
         }
 
-        // bulk lookup by guid
+        // bulk lookup by guid for families
         def data = doBulkLookup(familyGuids)
-
-        // inject extra metadata
         families.each { name, fam ->
-            def famData = data[name.toUpperCase()]
+            def famData = data[fam.guid]
             if (famData) {
                 fam.common = famData.common
-                fam.image = famData.image
+                if (!ConfigurationHolder.config.expert.images.useConstructedUrls) {
+                    fam.image = famData.image
+                }
             }
             else {
-                println "no data found for ${name}"
+                println "no common name found for ${name}"
             }
         }
+
+/*
+        // bulk lookup by guid for species
+        def spdata = doBulkLookup(speciesGuids)
+        families.each { name, fam ->
+            def spData = spdata[fam.anySpeciesGuid]
+            if (spData) {
+                fam.image = spData.image
+            }
+            else {
+                println "no image data found for ${name}"
+            }
+        }
+*/
 
         return families
     }
 
-    def doBulkLookup(list) {
+    def doBulkLookup(guids) {
         def url = ConfigurationHolder.config.bie.baseURL
+        println url
         def data = webService.doJsonPost(url,
-                "species/guids/bulklookup.json", "", (list as JSON).toString())
+                "species/guids/bulklookup.json", "", (guids as JSON).toString())
+        println data
         Map results = [:]
-        data.searchDTOList.each {family ->
-            def name = family.name
-            if (family.acceptedConceptName != 'null') { name = family.acceptedConceptName }
-            def image = [:]
-            results.put name.toString().toUpperCase(), [
-                    common: family.commonNameSingle,
-                    guid: family.guid,
-                    image: [largeImageUrl: family.largeImageUrl,
-                            smallImageUrl: family.smallImageUrl,
-                            thumbnailUrl: family.thumbnailUrl,
-                            imageMetadataUrl: family.imageMetadataUrl]]
+        data.searchDTOList.each {item ->
+            results.put item.guid, [
+                    common: item.commonNameSingle,
+                    image: [largeImageUrl: item.largeImageUrl,
+                            smallImageUrl: item.smallImageUrl,
+                            thumbnailUrl: item.thumbnailUrl,
+                            imageMetadataUrl: item.imageMetadataUrl]]
         }
         return results
     }
