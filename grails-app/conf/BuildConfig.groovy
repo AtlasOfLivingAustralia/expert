f6cb0cc6ac581bd6c24ea97f122e25041ed71a43
markew <mark.woolston@axon.com.au>
2013-01-13 03:02:06 +0000

    move plugin dependencies from application.properties to BuildConfig.groovy model BuildConfig on a fresh 2.1.2 project version
    git-svn-id: http://ala-expert.googlecode.com/svn/trunk/expert@33 acd0ad0d-b1c6-e151-42b4-198cfe2acdb2

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,18 +1,26 @@
+grails.servlet.version = "2.5" // Change depending on target container compliance (2.5 or 3.0)
 grails.project.class.dir = "target/classes"
 grails.project.test.class.dir = "target/test-classes"
 grails.project.test.reports.dir = "target/test-reports"
+grails.project.target.level = 1.6
+grails.project.source.level = 1.6
 //grails.project.war.file = "target/${appName}-${appVersion}.war"
+
 grails.project.dependency.resolution = {
     // inherit Grails' default dependencies
     inherits("global") {
         // uncomment to disable ehcache
         // excludes 'ehcache'
     }
-    log "warn" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
+    log "error" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
+
     repositories {
         grailsPlugins()
         grailsHome()
         grailsCentral()
+
+        mavenLocal()
+        mavenCentral()
 
         // uncomment the below to enable remote dependency resolution
         // from public Maven repositories
@@ -29,5 +37,23 @@ grails.project.dependency.resolution = {
 
         // runtime 'mysql:mysql-connector-java:5.1.13'
         runtime 'net.sf.opencsv:opencsv:2.1'
+    }
+
+    plugins {
+        runtime ":hibernate:$grailsVersion"
+        //runtime ":jquery:1.8.0"
+        runtime ":resources:1.1.6"
+        runtime ":rest:0.7"
+
+        // Uncomment these (or add new ones) to enable additional resources capabilities
+        //runtime ":zipped-resources:1.0"
+        //runtime ":cached-resources:1.0"
+        //runtime ":yui-minify-resources:0.1.4"
+
+        build ":tomcat:$grailsVersion"
+
+        runtime ":database-migration:1.2.1"
+
+        compile ':cache:1.0.1'
     }
 }
